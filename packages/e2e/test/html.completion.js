c8dcc7974d52e63a10cf854be83dd10fb9ae9fad
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-06-25 22:39:38 +0200

    convert to monorepo, add e2e tests (#1)

---
+++ packages/e2e/test/html.completion.js
@@ -0,0 +1,30 @@
+import { expect } from '@playwright/test'
+import { mkdtemp, writeFile } from 'fs/promises'
+import { join } from 'node:path'
+import { tmpdir } from 'os'
+import { runWithExtension, test } from '../src/runWithExtension.js'
+
+const getTmpDir = () => {
+  return mkdtemp(join(tmpdir(), 'css-completion'))
+}
+
+test('html.completion', async () => {
+  const tmpDir = await getTmpDir()
+  await writeFile(join(tmpDir, 'test.html'), '<')
+  const page = await runWithExtension({
+    folder: tmpDir,
+  })
+  const testHtml = page.locator('text=test.html')
+  await testHtml.click()
+  const tokenText = page.locator('.Token.Text')
+  await tokenText.click()
+  await page.keyboard.press('End')
+  await page.keyboard.press('Control+Space')
+
+  const completions = page.locator('#Completions')
+  await expect(completions).toBeVisible()
+
+  const completionItems = completions.locator('.EditorCompletionItem')
+  const completionItemOne = completionItems.nth(0)
+  await expect(completionItemOne).toHaveText('a')
+})
---
+++ packages/e2e/test/html.tab-completion.js
@@ -0,0 +1,53 @@
+import { expect } from '@playwright/test'
+import { mkdtemp, writeFile } from 'fs/promises'
+import { join } from 'node:path'
+import { tmpdir } from 'os'
+import { runWithExtension, test } from '../src/runWithExtension.js'
+
+const getTmpDir = () => {
+  return mkdtemp(join(tmpdir(), 'css-completion'))
+}
+
+const trimLines = (string) => {
+  return string.split('\n').join('')
+}
+
+test('html.tab-completion', async () => {
+  const tmpDir = await getTmpDir()
+  await writeFile(join(tmpDir, 'test.html'), '')
+  const page = await runWithExtension({
+    folder: tmpDir,
+  })
+  const testHtml = page.locator('text=test.html')
+  await testHtml.click()
+
+  const editorRow = page.locator('.EditorRow')
+  await editorRow.click({
+    position: {
+      x: 0,
+      y: 0,
+    },
+  })
+
+  const editorCursor = page.locator('.EditorCursor')
+  await expect(editorCursor).toHaveCount(1)
+  await expect(editorCursor).toHaveCSS('top', '0px')
+  await expect(editorCursor).toHaveCSS('left', '0px')
+
+  await page.keyboard.press('!')
+  await page.keyboard.press('Tab')
+
+  const editor = page.locator('.Editor')
+  await expect(editor).toHaveText(
+    trimLines(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Document</title>
+  </head>
+  <body>
+
+  </body>
+</html>`)
+  )
+})
