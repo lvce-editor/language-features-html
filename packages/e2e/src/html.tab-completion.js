06c6817b97c57ac7aca8ec26535e53676f11f5f4
Le Vivilet <levivlet@pm.me>
2022-08-19 11:40:03 +0200

    refactor e2e tests (wip)

--- packages/e2e/src/html.tab-completion.js
+++ packages/e2e/src/html.tab-completion.js
@@ -1,44 +1,19 @@
-import {
-  expect,
-  getTmpDir,
-  runWithExtension,
-  test,
-} from '@lvce-editor/test-with-playwright'
-import { writeFile } from 'fs/promises'
-import { join } from 'node:path'
-
 const trimLines = (string) => {
   return string.split('\n').join('')
 }
 
 test('html.tab-completion', async () => {
-  const tmpDir = await getTmpDir()
-  await writeFile(join(tmpDir, 'test.html'), '')
-  const page = await runWithExtension({
-    folder: tmpDir,
-  })
-  const testHtml = page.locator('text=test.html')
-  await testHtml.click()
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.html`, '!')
+  await Main.openUri(`${tmpDir}/test.html`)
+  await Editor.setCursor(0, 1)
 
-  const editorRow = page.locator('.EditorRow')
-  await editorRow.click({
-    position: {
-      x: 0,
-      y: 0,
-    },
-  })
+  // act
+  await Editor.executeTabCompletion()
 
-  const editorCursor = page.locator('.EditorCursor')
-  await expect(editorCursor).toHaveCount(1)
-  await expect(editorCursor).toHaveCSS('top', '0px')
-  await expect(editorCursor).toHaveCSS('left', '0px')
-
-  await page.keyboard.press('!')
-  const editor = page.locator('.Editor')
-  await expect(editor).toHaveText('!')
-
-  await page.keyboard.press('Tab')
-
+  // assert
+  const editor = Locator('.Editor')
   await expect(editor).toHaveText(
     trimLines(`<!DOCTYPE html>
 <html lang="en">
