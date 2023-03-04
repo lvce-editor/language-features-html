c1bb45f070bb28d2a10acf0041d0400922d3efbc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-04 20:41:29 +0100

    test: add css completion test (#46)

---
+++ packages/e2e/src/html.css-completion.js
@@ -0,0 +1,28 @@
+export const skip = true
+
+export const name = 'html.css-completion'
+
+export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.html`,
+    `<style>
+h1 {
+
+}
+  </style>`
+  )
+  await Main.openUri(`${tmpDir}/test.html`)
+  await Editor.setCursor(2, 0)
+
+  // act
+  await Editor.openCompletion()
+
+  // assert
+  const completions = Locator('#Completions')
+  await expect(completions).toBeVisible()
+  const completionItems = completions.locator('.EditorCompletionItem')
+  // TODO
+  await expect(completionItems.nth(0)).toHaveText('a')
+}
