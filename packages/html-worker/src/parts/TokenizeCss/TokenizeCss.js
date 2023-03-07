7bee4be149c1514b08ea3feac80688f4a1a34d86
Le Vivilet <levivlet@pm.me>
2023-03-07 13:46:40 +0100

    refactor: remove unused code

--- packages/html-worker/src/parts/TokenizeCss/TokenizeCss.js
+++ packages/html-worker/src/parts/TokenizeCss/TokenizeCss.js
@@ -115,22 +115,3 @@ export const tokenizeCss = (text) => {
   }
   return tokens
 }
-
-/**
- * @param {any[]} tokens
- * @param {number} offset
- * @returns {any}
- */
-export const getTokenAtOffset = (tokens, offset) => {
-  // TODO binary search
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i]
-    if (token.offset >= offset) {
-      if (token.offset === offset) {
-        return token
-      }
-      return tokens[i - 1]
-    }
-  }
-  return tokens[tokens.length - 1]
-}
