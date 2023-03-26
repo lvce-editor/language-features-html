b296512dd9b6f5f8621e6e0cbefb99210398735c
Le Vivilet <levivlet@pm.me>
2023-03-26 21:17:19 +0200

    refactor: sort imports

--- packages/extension/src/languageFeaturesHtmlMain.js
+++ packages/extension/src/languageFeaturesHtmlMain.js
@@ -1,6 +1,6 @@
+import * as ClosingTagProvider from './parts/ExtensionHostClosingTagProviderHtml/ExtensionHostClosingTagProviderHtml.js'
 import * as CompletionProvider from './parts/ExtensionHostCompletionProviderHtml/ExtensionHostCompletionProviderHtml.js'
 import * as TabCompletionProvider from './parts/ExtensionHostTabCompletionHtml/ExtensionHostTabCompletionHtml.js'
-import * as ClosingTagProvider from './parts/ExtensionHostClosingTagProviderHtml/ExtensionHostClosingTagProviderHtml.js'
 
 export const activate = () => {
   // TODO
