524a0166314ae1c02439053efb1e6f911e9b75ec
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-07 19:12:57 +0100

    feature: add script to generate html attributes json (#116)

--- packages/build/src/parts/UpdateHtmlAttributes/UpdateHtmlAttributes.js
+++ packages/build/src/parts/UpdateHtmlAttributes/UpdateHtmlAttributes.js
@@ -1,1 +1,32 @@
 // TODO update html attributes with vscode web custom data
+import htmlData from '@vscode/web-custom-data/data/browsers.html-data.json' assert { type: 'json' }
+import { join } from 'node:path'
+import * as JsonFile from '../JsonFile/JsonFile.js'
+import * as Root from '../Root/Root.js'
+
+const generateAttributesJson = (htmlData) => {
+  const { globalAttributes } = htmlData
+  const attributes = []
+  for (const globalAttribute of globalAttributes) {
+    attributes.push({
+      name: globalAttribute.name,
+    })
+  }
+  return {
+    globalAttributes: attributes,
+  }
+}
+
+const main = async () => {
+  const attributesJson = generateAttributesJson(htmlData)
+  const attributesJsonPath = join(
+    Root.root,
+    'packages',
+    'html-worker',
+    'data',
+    'html-attributes.json',
+  )
+  await JsonFile.writeJson(attributesJsonPath, attributesJson)
+}
+
+main()
