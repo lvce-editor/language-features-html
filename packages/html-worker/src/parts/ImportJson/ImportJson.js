a26fb0cb6987e6018518ab11df394335127d1c93
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-17 15:51:54 +0100

    feature: enable completion integration test (#123)

--- packages/html-worker/src/parts/ImportJson/ImportJson.js
+++ packages/html-worker/src/parts/ImportJson/ImportJson.js
@@ -1,21 +1,20 @@
 const importJsonNode = async (path) => {
   const fs = await import('fs/promises')
-  const data = await fs.readFile(path, 'utf8')
+  const { fileURLToPath } = await import('url')
+  const absoluteUri = new URL(`../../../${path}`, import.meta.url).toString()
+  const absolutePath = fileURLToPath(absoluteUri)
+  const data = await fs.readFile(absolutePath, 'utf8')
   return JSON.parse(data)
 }
 
 const importJsonBrowser = async (path) => {
-  try {
-    const absolutePath = import.meta.resolve(`../../../${path}`)
-    const response = await fetch(absolutePath)
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    const json = await response.json()
-    return json
-  } catch (error) {
-    throw new Error(`Failed to import ${path}: ${error}`)
+  const absolutePath = import.meta.resolve(`../../../${path}`)
+  const response = await fetch(absolutePath)
+  if (!response.ok) {
+    throw new Error(response.statusText)
   }
+  const json = await response.json()
+  return json
 }
 
 // TODO use import json once supported
