2632b792a512b5ef4bcb526bb7fdc41beb46fb23
Le Vivilet <levivilet@pm.me>
2026-09-21 05:07:47 +0000

    test: use printable wrap tag shortcut key

--- packages/e2e/src/html.wrap-tag.ts
+++ packages/e2e/src/html.wrap-tag.ts
@@ -10,7 +10,7 @@ export const test: Test = async ({ Editor, FileSystem, KeyBoard, Locator, Main }
   await Locator('.EditorInput textarea').click()
   await Editor.setSelections(new Uint32Array([0, 1, 0, 27]))
 
-  await KeyBoard.press('Alt+KeyW')
+  await KeyBoard.press('Alt+w')
 
   await Editor.shouldHaveText('<div><button>hello world</button></div>')
   await Editor.undo()
