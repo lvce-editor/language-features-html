87652292d16ffcba1a8435b4868fd48012327bcb
Le Vivilet <levivilet@pm.me>
2026-09-21 04:45:55 +0000

    test: focus editor before wrap tag shortcut

--- packages/e2e/src/html.wrap-tag.ts
+++ packages/e2e/src/html.wrap-tag.ts
@@ -2,11 +2,12 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'html.wrap-tag'
 
-export const test: Test = async ({ Editor, FileSystem, KeyBoard, Main }) => {
+export const test: Test = async ({ Editor, FileSystem, KeyBoard, Locator, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const uri = `${tmpDir}/test.html`
   await FileSystem.writeFile(uri, '<button>hello world</button>')
   await Main.openUri(uri)
+  await Locator('.EditorInput textarea').click()
   await Editor.setSelections(new Uint32Array([0, 1, 0, 27]))
 
   await KeyBoard.press('Alt+W')
